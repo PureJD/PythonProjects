f2d24daca3358ddefb5cb33e21ff70e113c36476
PureJD <6454jonny@gmail.com>
2024-01-28 14:25:36 +0000

    This game has now been completed and many features have been added and perfected. The readme file had been updated

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -1,43 +1,62 @@
 from random import randint
+import json
 
-def player_name():
-    return input('Please enter playername: ')
+def load_scores(filename='scores.json'):
+    try:
+        with open(filename, 'r') as file:
+            scores = json.load(file)
+    except FileNotFoundError:
+        scores = {}
+    return scores
+
+def save_scores(scores, filename='scores.json'):
+    with open(filename, 'w') as file:
+        json.dump(scores, file)
+
+def update_scores(player, score, scores):
+    if player in scores:
+        scores[player] += score
+    else:
+        scores[player] = score
+
+def input_player_name():
+    return input('Please enter player name: ')
 
 def input_from_user():
-    range_to = input('This program will set a value and it is up to you to guess that value. The program will select a number from the number 1 up until the number: ')
+    range_to = input('This program will set a value, and it is up to you to guess that value. Enter the upper limit for the range (a positive integer): ')
     if range_to.isdigit():
-        print(f'Thank you, now let me think...I have selected a number beteen 1 and {range_to} ')
+        print(f'Thank you, now let me think... I have selected a number between 1 and {range_to}.')
+        return int(range_to)
     else:
-        print('You do not seem to have entere a vaid number')
-        input_from_user()
-    return range_to
+        print('You did not enter a valid number.')
+        return input_from_user()
 
-def correct_answer():
+def correct_answer(random_number):
     attempts_count = 1
     while True:
         user_guess = input('What is your guess?: ')
         if int(user_guess) == random_number:
-            print('Wow, what are the odds? Whatever they were you have beaten them. Congratulations')
-            print(f'That has taken you {attempts_count} tries. Do you think you can beat your previous score?')
+            print(f'Congratulations! You guessed the correct number in {attempts_count} tries.')
             return attempts_count
         else:
             print('Nope')
             attempts_count += 1
-            continue
-        
 
-player_user_name = player_name()
-scoreboard = {}
+scores = load_scores()
+print(f'The current HIGHSCORES for this are {scores}, however as you choose your own difficulty, take this with a pinch of salt')
+
+player_name = input_player_name()
 
 while True:
-    user_input = input_from_user()
-    random_number = randint(1, int(user_input))
-    previous_score  = correct_answer()
-    statistics = 'A number between 1 and' + user_input
+    range_limit = input_from_user()
+    random_number = randint(1, range_limit)
 
+    current_score = correct_answer(random_number)
+    update_scores(player_name, current_score, scores)
+    
+    save_scores(scores)
 
-#The plan for this program is you lose your place next time is to: create new function new game plus which will let users play again but also record username, difficulty and score in the dictionary. The main game loop needs to be adjusted.
-
-
-
+    play_again = input("Do you want to play again? (yes/no): ")
+    if play_again.lower() != 'yes':
+        break
 
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # PythonProjects
-A collection of smaller projects programmed in Python. This collection has been added to a single repository to prevent clutter. 
+### A collection of smaller projects programmed in Python. This collection has been added to a single repository to prevent clutter.
+![Hard at work](https://www.google.com/url?sa=i&url=https%3A%2F%2Fimgur.com%2Fgallery%2FBhP6oIP&psig=AOvVaw0VaNR-SPDdT2I80EZgzIOj&ust=1706537666529000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCIj9ja-igIQDFQAAAAAdAAAAABAJ)
+
+# Number Guessing Game
+### - A game in which the program takes your name and a range of values. It then generates a random number between your values and you attempt to guess the answer.
+### - This program has also had an additional scoreboard feature added in which it will read a JSON file, write to the file your username and score and then save the file for future users of the game. 
+
+
+## All of these projects utilise original design and programming code. Please feel free to take any concepts used here and add them to your own projects. 
+
+
+![GitHub Logo](https://github.com/github.png)
