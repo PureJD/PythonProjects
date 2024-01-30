e916945e10704a574e00e9390abb0bb050fbb77b
PureJD <6454jonny@gmail.com>
2024-01-30 17:17:28 +0000

    Additional art work

--- TurtleArt.py
+++ TurtleArt.py
@@ -1,30 +1,39 @@
-import turtle as t
-from random import random
+import turtle
 
-for i in range(10000):
-    t.bgcolor('black')
-    t.width(10)
-    t.color('cyan')
-    steps = int(random() * 100)
-    angle = int(random() * 360)
-    t.right(angle)
-    t.fd(steps) 
-    t.speed(1000)
+s = turtle.Screen()
+turtle.bgcolor('gray')
+t = turtle.Turtle()
 
-    for i in range(1):
-        t.bgcolor('black')
-        t.width(10)
-        t.color('red')
-        steps = int(random() * 100)
-        angle = int(random() * 360)
-        t.right(angle)
-        t.fd(steps)
+# Define a function to draw a figure
+def drawDavid(t):
+    # Move the turtle to an initial angle
+    t.left(120)
 
-        for i in range(1):
-            t.bgcolor('black')
-            t.width(10)
-            t.color('green')
-            steps = int(random() * 100)
-            angle = int(random() * 360)
-            t.right(angle)
-            t.fd(steps)
+    # Draw the first set of three lines in the shape of a triangle
+    for i in range(0, 3):
+       
+        t.forward(100)  # Move the turtle forward by 100 units
+        t.right(120)    # Turn the turtle right by 120 degrees
+
+    # Lift the pen (stop drawing)
+    t.penup()
+
+    # Move the turtle to a new position
+    t.goto(-50, 30)
+
+    # Put the pen down (start drawing again)
+    t.pendown()
+
+    # Rotate the turtle to a new angle
+    t.right(120)
+
+    # Draw the second set of three lines in the shape of an inverted triangle
+    for i in range(0, 3):
+        t.forward(100)  # Move the turtle forward by 100 units
+        t.left(120)     # Turn the turtle left by 120 degrees
+
+    # Indicate that the drawing is done
+    turtle.done()
+
+# Call the function to draw the figure using the turtle object 't'
+drawDavid(t)
